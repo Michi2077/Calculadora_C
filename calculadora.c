f501facb3abfbdaf520eb192e71f4823a14eb139
s4ulvega <166859049+s4ulvega@users.noreply.github.com>
2024-09-03 13:02:12 -0600

    Update calculadora.c
    funcion division

--- calculadora.c
+++ calculadora.c
@@ -7,3 +7,12 @@ void suma(float a, float b) {
 void resta(float a, float b) {
     printf("Resultado: %.2f\n", a - b);
 }
+
+float division(float a, float b){
+    if(b != 0){
+         printf("Resultado: %.2f\n", a/b);
+    }else{
+        printf("ERROR!! DIVISION POR CERO NO PERMITIDA\n");
+    }
+    return 0;
+}
