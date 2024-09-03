7aa3be458011b3858eac773e62b1c1b5aa74d7a4
s4ulvega <166859049+s4ulvega@users.noreply.github.com>
2024-09-03 13:25:03 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,18 +1,7 @@
-#include <stdio.h>
-
-void suma(float a, float b) {
-    printf("Resultado: %.2f\n", a + b);
-}
-
-void resta(float a, float b) {
-    printf("Resultado: %.2f\n", a - b);
-}
-
 float division(float a, float b){
     if(b != 0){
          printf("Resultado: %.2f\n", a/b);
     }else{
         printf("ERROR!! DIVISION POR CERO NO PERMITIDA\n");
     }
-    return 0;
 }
