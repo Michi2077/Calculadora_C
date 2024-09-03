64e68a8c994f516e8f09d959993004e94f46f2dd
Flores Michi Santiago Sebastian <167127503+Michi2077@users.noreply.github.com>
2024-09-03 13:08:22 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -14,5 +14,52 @@ float division(float a, float b){
     }else{
         printf("ERROR!! DIVISION POR CERO NO PERMITIDA\n");
     }
+}
+
+int main() {
+    float num1, num2;
+    int opcion;
+
+    while (1) {
+        printf("\tCalculadora\n");
+        printf("1- + Suma\n");
+        printf("2- - Resta\n");
+        printf("3- * Multiplicación\n");
+        printf("4- / División\n");
+        printf("5- Salir\n");
+        printf("Elige una opción: ");
+        scanf("%d", &opcion);
+
+        if (opcion == 5) {
+            break;
+        }
+
+        printf("\nIntroduce el primer número: ");
+        scanf("%f", &num1);
+        printf("Introduce el segundo número: ");
+        scanf("%f", &num2);
+
+        switch (opcion) {
+            case 1:
+                suma(num1, num2);
+                printf("\n");
+                break;
+            case 2:
+                resta(num1, num2);
+                printf("\n");
+                break;
+            case 3:
+                multiplicacion(num1, num2);
+                printf("\n");
+                break;
+            case 4:
+                division(num1, num2);
+                printf("\n");
+                break;
+            default:
+                printf("Opción no válida.\n");
+        }
+    }
+
     return 0;
 }
